38ce2307be9c5bc4f42db85f7d212ddad4560d0b
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2018-10-06 21:36:43 +0300

    herokuuu

--- src/main/java/tikape/harjoitustyo/database/AiheDao.java
+++ src/main/java/tikape/harjoitustyo/database/AiheDao.java
@@ -134,7 +134,7 @@ public class AiheDao implements Dao<Aihe, Integer>{
     
     public Aihe findAiheWithName(Integer kurssi_id, String aiheennimi, String kysymys) throws SQLException {
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Aihe WHERE aiheenNimi ILIKE ?");
-        stmt.setString(1, aiheennimi));
+        stmt.setString(1, aiheennimi);
         Aihe a;
         Kysymys k;
         KysymysDao kyssaridao = new KysymysDao(this.connection);
